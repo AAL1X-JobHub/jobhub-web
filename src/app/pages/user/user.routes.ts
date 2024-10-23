82cc07a2a57144268aa153f694f294ea995db386
superleo18 <leonardobravoricapa@gmail.com>
2024-10-23 13:08:28 -0500

    Creación de los guards para auth

---
+++ src/app/pages/user/user.routes.ts
@@ -0,0 +1,26 @@
+import { Routes } from '@angular/router';
+
+import { DirectoryComponent } from './directory/directory.component';
+import { ApplicationsComponent } from './applications/applications.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { LayoutComponent } from './layout/layout.component';
+
+import { UpdateProfileComponent } from '../../shared/components/update-profile/update-profile.component';
+import { UserProfileComponent } from '../../shared/components/user-profile/user-profile.component';
+
+
+export const customerRoutes: Routes = [
+  {
+    path: '',
+    component: LayoutComponent,
+    children: [
+      { path: 'directory', component: DirectoryComponent },
+      { path: 'applications', component: ApplicationsComponent },
+      { path: 'portfolio', component: PortfolioComponent },
+      { path: 'profile', component: UserProfileComponent },
+      { path: 'profile/update', component: UpdateProfileComponent },
+      // Agrega más rutas según tus necesidades
+     // { path: '', redirectTo: 'catalog', pathMatch: 'full' } // Redirigir a catalog por defecto
+    ]
+  }
+];
--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
+import { AuthService } from '../../../core/services/auth.service';
 
 @Component({
   selector: 'app-navbar',
@@ -9,7 +10,14 @@ import { RouterLink, RouterOutlet } from '@angular/router';
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
-  
+  private authService = inject(AuthService);
+
+  isAuthenticated: boolean = false;
+
+  ngOnInit(): void {
+    this.isAuthenticated = this.authService.isAuthenticated();
+  }
+
   logout(): void{}
 
 }
